dfe1e8c5ddec7f21280d1540b96a50417cba5114
Braian Hercun <94329348+HercunBraian@users.noreply.github.com>
2022-11-19 16:32:27 -0300

    Se agrega el desactivar o activar y el borrar menu

--- src/components/Admin/Menu/MenuItem/MenuItem.js
+++ src/components/Admin/Menu/MenuItem/MenuItem.js
@@ -4,7 +4,7 @@ import {Icon, Button, Confirm} from "semantic-ui-react";
 import {BasicModal} from "../../../Admin/Shared";
 import {Menu} from "../../../../api";
 import {useAuth} from "../../../../hooks";
-import {MenuForm} from "../MenuForm"
+import {MenuForm} from "../MenuForm";
 
 const menuController = new Menu();
 
@@ -15,14 +15,59 @@ export function MenuItem(props) {
   const [showModal, setShowModal] = useState(false);
   const [titleModal, setTitleModal] = useState("");
 
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [confirmMessage, setConfirmMessage] = useState("");
+  const [isDelete, setisDelete] = useState(false)
+
   const {accessToken} = useAuth();
-  
+
   //Funcion para abrir o cerrar el modal
   const onOpenCloseModal = () => setShowModal ((prevState) => !prevState);
+
+  //Funcion para la confirmacion
+  const onOpenCloseConfirm = () => setShowConfirm ((prevState) => !prevState);
 
   const onOpenUpdateMenu = () => {
     setTitleModal(`Actualizar menu: ${menu.title}`);
     onOpenCloseModal();
+  }
+
+  const openDesactivateActiveConfirm = () =>{
+    setisDelete(false);
+    setConfirmMessage(
+      menu.active ? `Desactivar el menu ${menu.title}` : `Activar el menu ${menu.title}`
+    );
+
+    onOpenCloseConfirm();
+  }
+
+  const onActivateDesactivate = async () => {
+    try {
+      await menuController.updateMenu(accessToken, menu._id, {
+        active: !menu.active,
+      } )
+
+      onReload();
+      onOpenCloseConfirm();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const openDeleteConfirm = () => {
+    setisDelete(true);
+    setConfirmMessage(`Eliminar el menu ${menu.title}`);
+    onOpenCloseConfirm();
+  }
+
+  const onDelete = async () => {
+    try {
+      await menuController.deleteMenu(accessToken, menu._id);
+      onReload();
+      onOpenCloseConfirm();
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <>
@@ -37,11 +82,11 @@ export function MenuItem(props) {
             <Icon name="pencil" onClick={onOpenUpdateMenu} />
           </Button>
 
-          <Button icon color={menu.active ? "orange" : "teal"}>
+          <Button icon color={menu.active ? "orange" : "teal"} onClick={openDesactivateActiveConfirm}>
             <Icon name={menu.active ? "ban" : "check"} />
           </Button>
 
-          <Button icon color="red">
+          <Button icon color="red" onClick={openDeleteConfirm}>
             <Icon name="trash" />
           </Button>
         </div>
@@ -50,6 +95,15 @@ export function MenuItem(props) {
       <BasicModal show={showModal} close={onOpenCloseModal} title={titleModal}>
         <MenuForm onClose={onOpenCloseModal} onReload={onReload} menu={menu}  />
       </BasicModal>
+
+      <Confirm 
+      open={showConfirm}
+      onCancel={onOpenCloseConfirm}
+      onConfirm={isDelete ? onDelete : onActivateDesactivate}
+      content={confirmMessage}
+      size="mini"
+
+      />
     </>
   )
 }
