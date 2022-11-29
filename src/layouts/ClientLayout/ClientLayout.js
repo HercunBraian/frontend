8c501d497793ea0961555d0c5d2f1830e0d37ae4
Braian Hercun <94329348+HercunBraian@users.noreply.github.com>
2022-11-29 20:48:49 -0300

    Comienza el layout de Web

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import {Container} from "semantic-ui-react";
+import "./ClientLayout.scss";
+import {TopBar} from "../../components/Web";
 
 export function ClientLayout(props) {
   const {children} = props;
   return (
-    <div>
-        <h1>Estamos cargando el ClientLayout</h1>
-
+    <div className='client-layout'>
+        <div className='client-layout__header'>
+          <TopBar />
+        </div>
         {children}
+
+        <div className='client-layout__footer'>
+        <Container>
+            <span>INFO</span>
+            <span>MENU</span>
+            <span>NEWSLETTER</span>
+          </Container>
+          <Container>
+            <span>@ ALL RIGHTS RESERVED</span>
+            <span>BRAIAN DANIERL HERCUN | FULLSTACK MERN </span>
+          </Container>
+        </div>
     </div>
   )
 }
---
+++ src/utils/social-data.js
@@ -0,0 +1,18 @@
+export const socialData = [
+    {
+        type: "youtube",
+        link: "https://www.youtube.com"
+    },
+    {
+        type: "twitter",
+        link: "https://www.twitter.com"
+    },
+    {
+        type: "facebook",
+        link: "https://www.facebook.com"
+    },
+    {
+        type: "linkedin",
+        link: "https://www.linkedin.com"
+    },
+]
